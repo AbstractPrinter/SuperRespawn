208b2136daa97830ef0228b524afce102b3bea3a
AbstractPrinter <wangyudi007@gmail.com>
2021-01-21 14:47:04 +0800

    修复 @AnsonSkywalker 提交的问题:https://github.com/AbstractPrinter/SuperRespawn/issues/1

--- src/main/java/cn/wangyudi/superrespawn/listeners/RespawnListener.java
+++ src/main/java/cn/wangyudi/superrespawn/listeners/RespawnListener.java
@@ -28,44 +28,72 @@ public class RespawnListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onNearestSpawn(PlayerRespawnEvent event) {
-        String respawnName = null;
-        Location respawnLocation = null;
-        if ("nearest".equals(plugin.getConfig().getString("mode"))) {
-            double min = 999999999;
+    public void onRespawn(PlayerRespawnEvent event) {
+        try {
+            String mode = plugin.getConfig().getString("mode");
+            String respawnKey = null;
+            boolean isChat = plugin.getConfig().getBoolean("prompt.chat");
+            boolean isTitle = plugin.getConfig().getBoolean("prompt.title");
             Player player = event.getPlayer();
-            for (Map.Entry<String, Location> spawnLocationsEntry : respawnLocations.entrySet()) {
-                if (player.hasPermission(String.format("superrespawn.spawn.%s", spawnLocationsEntry.getKey())) && player.getWorld().equals(spawnLocationsEntry.getValue().getWorld()) && min > spawnLocationsEntry.getValue().distance(player.getLocation())) {
-                    min = spawnLocationsEntry.getValue().distance(player.getLocation());
-                    respawnName = spawnLocationsEntry.getKey();
-                    respawnLocation = spawnLocationsEntry.getValue();
-                }
+            Location respawnLoc = null;
+            switch (mode) {
+                // 根据最近传送点重生
+                case "nearest":
+                    double minLength = 2147483647;
+                    for (Map.Entry<String, Location> entry : this.respawnLocations.entrySet()) {
+                        if (player.getLocation().distance(entry.getValue()) < minLength) {
+                            respawnKey = entry.getKey();
+                            respawnLoc = entry.getValue();
+                        }
+                    }
+                    break;
+                // 根据权重重生
+                case "weight":
+                    if (this.respawnWeights.size() > 0) {
+                        int max = 0;
+                        String maxKey = null;
+                        for (Map.Entry<String, Integer> entry : this.respawnWeights.entrySet()) {
+                            if (max < entry.getValue()) {
+                                max = entry.getValue();
+                                maxKey = entry.getKey();
+                            }
+                        }
+                        respawnKey = maxKey;
+                        respawnLoc = this.respawnLocations.get(maxKey);
+                    } else {
+                        player.sendMessage("似乎没有设置过权重的重生点!");
+                    }
+                    break;
+                // mode项错填或者填写了 default
+                default:
+                    respawnKey = "default";
+                    respawnLoc = event.getRespawnLocation();
             }
-        }
-        if ("weight".equals(plugin.getConfig().getString("mode"))) {
-            int max = 0;
-            Player player = event.getPlayer();
-            for (Map.Entry<String, Integer> spawnWeightsEntry : respawnWeights.entrySet()) {
-                if (player.hasPermission(String.format("superrespawn.spawn.%s", spawnWeightsEntry.getKey())) && player.getWorld().equals(respawnLocations.get(spawnWeightsEntry.getKey()).getWorld()) && max < spawnWeightsEntry.getValue()) {
-                    max = spawnWeightsEntry.getValue();
-                    respawnName = spawnWeightsEntry.getKey();
-                    respawnLocation = respawnLocations.get(spawnWeightsEntry.getKey());
-                }
-            }
-        }
-        if ("default".equals(plugin.getConfig().getString("mode"))) {
-            Player player = event.getPlayer();
-            respawnLocation = player.getWorld().getSpawnLocation();
-        }
-        if (plugin.getConfig().getBoolean("prompt.chat")) {
-            Player player = event.getPlayer();
-            player.sendMessage(respawnPrompts.get(respawnName)[0]);
-        }
-        if (plugin.getConfig().getBoolean("prompt.title")) {
-            Player player = event.getPlayer();
-            player.sendTitle(respawnPrompts.get(respawnName)[1], respawnPrompts.get(respawnName)[2],10,70,20);
-        }
-        event.setRespawnLocation(respawnLocation);
 
+            String respawnDefault = "default";
+            assert respawnKey != null;
+            if (respawnKey.equals(respawnDefault)) {
+                if (isChat) {
+                    player.sendMessage(this.respawnPrompts.get(respawnKey)[0]);
+                }
+                if (isTitle) {
+                    player.sendTitle(this.respawnPrompts.get(respawnKey)[1], this.respawnPrompts.get(respawnKey)[1], 10, 70, 20);
+                }
+            } else {
+                if (isChat) {
+                    player.sendMessage("Default");
+                }
+                if (isTitle) {
+                    player.sendTitle("Default", "默认出生点", 10, 70, 20);
+                }
+            }
+            event.setRespawnLocation(respawnLoc);
+        } catch (Exception e) {
+            event.getPlayer().sendMessage("因为出现异常，所以本次重生事件并没有被插件接管，请联系管理员检查出现的问题");
+            plugin.getLogger().info(e.getMessage());
+            plugin.getLogger().info("出现异常，请在本项目 Github Issues 提交本次出错信息");
+            plugin.getLogger().info("https://github.com/AbstractPrinter/SuperRespawn/issues");
+            plugin.getLogger().info("请附上异常信息，感谢你为这个插件做出的贡献！");
+        }
     }
 }
